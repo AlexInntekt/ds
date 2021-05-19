b70e86fe4d336c71065f36984cab386d039c7979
Alex Manolescu <alexandru.manolescu@softbinator.com>
2021-05-19 22:07:45 +0300

    s

---
+++ src/api/urls.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import UsersView
+urlpatterns = [
+	path('/users', UsersView.as_view(), name="list_users"),
+]
--- src/ds/settings.py
+++ src/ds/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
     'rest_framework.authtoken',
+    'rest_auth',
 
-    'authen',
+    'api',
 ]
 
 MIDDLEWARE = [
